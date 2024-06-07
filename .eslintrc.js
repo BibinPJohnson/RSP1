9bd69a1e5efc7b10f32f06b61adb53deaa9a72ac
BibinPJohnson <90170319+BibinPJohnson@users.noreply.github.com>
2024-06-07 16:05:17 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,14 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'off',
+    // '@typescript-eslint/no-unused-vars': isProduction ? 'error' : 'warn',
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // 'no-console': isProduction ? 'error' : 'warn',
+    // 'eol-last': ['error', 'always'],
+    'simple-import-sort/imports': isProduction ? 'error' : 'warn',
+    // '@typescript-eslint/no-implicit-any-catch': 'off',
+    // 'dot-notation': 'warn',
+  },
 }
